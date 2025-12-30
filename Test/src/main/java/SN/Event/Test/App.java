67f618e226fb91cd1180944dc8eed7a41fa7005b
anish.guggilla <anish.guggilla@192.168.0.107>
2025-12-30 13:28:03 +0530

    30/12/2025-001

---
+++ Test/src/main/java/SN/Event/Test/App.java
@@ -0,0 +1,13 @@
+package SN.Event.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

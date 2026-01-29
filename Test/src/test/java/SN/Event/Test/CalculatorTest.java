9ad3983344278e40d82de2e3f4454fc3c4a1f626
1539Ani <125135913+1539Ani@users.noreply.github.com>
2026-01-29 15:53:12 +0530

    Update CalculatorTest.java

--- Test/src/test/java/SN/Event/Test/CalculatorTest.java
+++ Test/src/test/java/SN/Event/Test/CalculatorTest.java
@@ -1,41 +1,41 @@
-package SN.Event.Test;
+// package SN.Event.Test;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+// class CalculatorTest {
 
-    Calculator calc = new Calculator();
+//     Calculator calc = new Calculator();
 
-    @Test
-    void testAdd() {
-        assertEquals(15, calc.add(10, 5));
-        assertEquals(0, calc.add(-5, 5));
-    }
+//     @Test
+//     void testAdd() {
+//         assertEquals(15, calc.add(10, 5));
+//         assertEquals(0, calc.add(-5, 5));
+//     }
 
-    @Test
-    void testSubtract() {
-        assertEquals(5, calc.subtract(10, 5));
-        assertEquals(-10, calc.subtract(-5, 5));
-    }
+//     @Test
+//     void testSubtract() {
+//         assertEquals(5, calc.subtract(10, 5));
+//         assertEquals(-10, calc.subtract(-5, 5));
+//     }
 
-    @Test
-    void testMax() {
-        assertEquals(10, calc.max(10, 5));
-        assertEquals(5, calc.max(5, 5));
-        assertEquals(20, calc.max(15, 20));
-    }
+//     @Test
+//     void testMax() {
+//         assertEquals(10, calc.max(10, 5));
+//         assertEquals(5, calc.max(5, 5));
+//         assertEquals(20, calc.max(15, 20));
+//     }
 
-    @Test
-    void testDivide() {
-        assertEquals(2, calc.divide(10, 5));
-        assertEquals(-2, calc.divide(-10, 5));
-    }
+//     @Test
+//     void testDivide() {
+//         assertEquals(2, calc.divide(10, 5));
+//         assertEquals(-2, calc.divide(-10, 5));
+//     }
 
-    @Test
-    void testDivideByZero() {
-        assertThrows(IllegalArgumentException.class, () -> calc.divide(10, 0));
-    }
+//     @Test
+//     void testDivideByZero() {
+//         assertThrows(IllegalArgumentException.class, () -> calc.divide(10, 0));
+//     }
 
-}
+// }
